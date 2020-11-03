80dc05029309a6ff3e17f98c2eb1b7fb80310501
Logi-Way <web@wmg.co.il>
2020-11-03 09:33:22 +0200

    zip

--- src/app/models/frequency.ts
+++ src/app/models/frequency.ts
@@ -4,5 +4,6 @@ export class Frequency{
 }
 export const frequencies:Frequency[]=[
  {"id":1,"frequency_name":"חד-פעמי"},
- {"id":1,"frequency_name":"שבועי"}
+ {"id":2,"frequency_name":"שבועי"}
+ {"id":3,"frequency_name":"מותאם אישית"}
 ]
